e15fcbca5cac463eb590dcb45f6d7adabb9716c9
Luan <luanh3nrique08@gmail.com>
2023-12-28 10:23:10 -0300

    alterações

--- code/script.js
+++ code/script.js
@@ -1,15 +1,16 @@
-function carregar(){
+function carregar() {
 var msg = window.document.getElementById('msg')
-var image = window.document.getElementById('foto')
+var img = window.document.getElementById('foto')
 
 var data = new Date()
-var hora = data.getHours()
+//var hora = data.getHours()
+var hora = 18;
 msg.innerHTML = `Agora são ${hora} horas`
 }
 if (hora >= 0 && hora < 12){
-    image.src = "amanhecer.png"
-} else if (hora >= 12 && hora < 18){
-    image.src = "entardecer.png"
+    img.src = "amanhecer.png"
+} else if (hora >= 12 && hora <= 18){
+    img.src = "entardecer.png"
 } else {
-    image.src = "anoitecer.png"
+    img.src = "anoitecer.png"
 }
