9906ba8c50c11c281462394a9101b87b0071c51a
Luan <luanh3nrique08@gmail.com>
2023-12-28 10:11:39 -0300

    Update script.js

--- code/script.js
+++ code/script.js
@@ -3,6 +3,13 @@ var msg = window.document.getElementById('msg')
 var image = window.document.getElementById('foto')
 
 var data = new Date()
-var hora = data.getHours
+var hora = data.getHours()
 msg.innerHTML = `Agora são ${hora} horas`
+}
+if (hora >= 0 && hora < 12){
+    image.src = "amanhecer.png"
+} else if (hora >= 12 && hora < 18){
+    image.src = "entardecer.png"
+} else {
+    image.src = "anoitecer.png"
 }
